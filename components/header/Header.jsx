5643780bb4ac8aa649851853a35941190be7eabb
MossaJehad <Moosa.jehad65@gmail.com>
2025-08-06 18:18:46 +0300

    fixied the Console errors

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header({ activeTab, setActiveTab }) {
 	return (
 		<header className="px-8">
 			<section className="flex flex-row items-center justify-center align-center gap-4 p-8">
-				<Image src="/Sajilni.webp" width={100} height={100} alt="Sajilni" />
+				<Image src="/Sajilni.webp" width={100} height={100} alt="Sajilni" className="w-24 h-auto" priority/>
 				<SearchInput />
 				<ProfileChip />
 			</section>
